3060ceb5ca4919151e0d829f6ba70dfe183c8791
vikashjman <vikashkumar@jmangroup.com>
2024-02-27 22:55:22 +0530

    Service Logic Completed

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,13 +1,39 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Param } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './users.entity';
 @Injectable()
 export class UsersService {
-    constructor(@InjectRepository(User) private repo: Repository<User>) {}
+    constructor(@InjectRepository(User) private repo: Repository<User>) { }
 
-    create(email: string, password: string){
-        const user = this.repo.create({email, password});
+    create(email: string, password: string) {
+        const user = this.repo.create({ email, password });
         return this.repo.save(user);
+    }
+
+    findOne(id: number) {
+        const user = this.repo.findOneBy({ id });
+        return user;
+    }
+
+    find(email: string) {
+        return this.repo.find({ where: { email } });
+    }
+
+    async update(id: number, attrs: Partial<User>) {
+        const user = await this.repo.findOneBy({ id });
+        if (!user) {
+            throw new Error('user not found!');
+        }
+        Object.assign(user, attrs);
+        return this.repo.save(user);
+    }
+
+    async remove(id: number) {
+        const user = await this.repo.findOneBy({ id });
+        if (!user) {
+            throw new Error('user not found!');
+        }
+        return this.repo.remove(user);
     }
 }
