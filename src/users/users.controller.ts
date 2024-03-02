8c22fecdbf4c7f1133beb3f1bd17b88c705f190c
vikashjman <vikashkumar@jmangroup.com>
2024-03-02 18:44:01 +0530

    solved the issue of password in response, using interceptors logic

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException } from '@nestjs/common';
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete, NotFoundException, UseInterceptors, ClassSerializerInterceptor } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UdpateUserDto } from './dtos/update-user.dto';
@@ -12,12 +12,13 @@ export class UsersController {
         this.usersService.create(body.email, body.password);
     }
 
+    @UseInterceptors(ClassSerializerInterceptor)
     @Get('/:id')
     async findUser(@Param('id') id: string) {
         const user =  await this.usersService.findOne(parseInt(id));
         if(!user) throw new NotFoundException('user not found')
         return user;
-    }
+    } 
 
     @Get()
     findAllUsers(@Query('email') email:string){
